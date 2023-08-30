68bf2df2990a948f6cc196a409ca2705dc56a7f5
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-08-30 15:23:35 +0300

    1

---
+++ js/compression-operation.js
@@ -0,0 +1,34 @@
+// 'use strict'
+
+/*
+== не строге порівняння
+=== строге порівняння
+> < >= <=
+!=
+!==
+
+*/
+
+/*
+boolean: true / false  1 / 0
+'' = 0
+'a' = 1
+null = false = 0
+undefined = 0
+
+*/
+
+//console.log(object);
+
+//console.log('object :>> ', object);
+
+//console.table(object);
+
+//console.log();
+
+//console.log(10 === 10);
+//console.log(10 == 10);
+//console.table(10 === '10');
+//console.table(10 == '10');
+
+//console.error(10);
---
+++ js/variables.js
@@ -0,0 +1,18 @@
+//let - можна перевизначити
+//const - не можна перевизначити
+
+
+//const a = 123 ;
+//console.log(a);
+
+//let number = 10;
+//console.log(number);
+//number = 20;
+//console.log(number);
+
+//let a;
+//console.log(a);
+
+//const userFirstName = prompt('Enter your name');
+//console.log(userFirstName);
+//alert('Hello, ' + userFirstName);
